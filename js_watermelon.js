6a9529f812c7f0a9cec3732c05b0b91a02886a47
Sae-byeol <toquf0424@naver.com>
2023-04-30 18:50:33 +0900

    수박수박수박수?

---
+++ js_watermelon.js
@@ -0,0 +1,13 @@
+// 수박수박수박수?
+
+function solution(n) {
+    var answer = '';
+    for (var i=1;i<=n;i++){
+        if ( i % 2 ===0){
+            answer+='박';
+        }else{
+            answer += '수';
+        }
+    }
+    return answer;
+}
