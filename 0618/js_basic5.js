a89078b436b55a8ccec48ba901206167a42ec788
Sae-byeol <toquf0424@naver.com>
2023-06-19 02:50:01 +0900

    공 던지기

---
+++ 0618/js_basic5.js
@@ -0,0 +1,10 @@
+//  공던지기
+function solution(numbers, k) {
+    var answer = 0;
+
+    for (let i=0;i<k-1;i++){
+        answer = (answer+2)% numbers.length;
+       
+    }
+    return answer + 1;
+}
